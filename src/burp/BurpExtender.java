219fa51bc34503e89aedd8e9622655d06fe31476
August Detlefsen <august@Runamok.local>
2015-08-29 02:57:08 -0700

    Implement new PassiveScan API

--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -1,12 +1,12 @@
 package burp;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collections;
+import com.codemagi.burp.PassiveScan;
+import com.codemagi.burp.MatchRule;
+import com.codemagi.burp.ScanIssue;
+import com.codemagi.burp.ScanIssueConfidence;
+import com.codemagi.burp.ScanIssueSeverity;
+import com.codemagi.burp.ScannerMatch;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -20,16 +20,119 @@ import java.util.regex.Pattern;
  * @author August Detlefsen <augustd at codemagi dot com>
  * @contributor Thomas Dosedel <thom at secureideas dot com>
  */
-public class BurpExtender implements IBurpExtender, IScannerCheck {
+public class BurpExtender extends PassiveScan {
 
-    private IBurpExtenderCallbacks callbacks;
-    private IExtensionHelpers helpers;
-    private OutputStream output;
+    @Override
+    protected void initialize() {
+	//call the superclass initializer
+	super.initialize();
+	
+	//set the extension Name
+	extensionName = "Software Version Checks";
+	
+	//create match rules
+	addMatchRule(new MatchRule(Alterian_CME, 1, "Alterian-CME", ScanIssueSeverity.LOW));
+        addMatchRule(new MatchRule(ARR, 1, "IIS Application Request Routing", ScanIssueSeverity.LOW));
+        addMatchRule(new MatchRule(ASP_NET, 1, "ASP.Net", ScanIssueSeverity.LOW));
+	addMatchRule(new MatchRule(APACHE, 1, "Apache", ScanIssueSeverity.LOW));
+	addMatchRule(new MatchRule(APACHE_COYOTE, 1, "Apache Coyote (Tomcat)", ScanIssueSeverity.LOW));
+	addMatchRule(new MatchRule(APACHE_TOMCAT, 1, "Apache Tomcat", ScanIssueSeverity.LOW));
+	addMatchRule(new MatchRule(BOA, 1, "BOA Web Server", ScanIssueSeverity.LOW));
+	addMatchRule(new MatchRule(DOT_NET_FRAMEWORK, 1, "Microsoft .Net Framework", ScanIssueSeverity.LOW));
+	addMatchRule(new MatchRule(DOT_NET_FRAMEWORK_SDK, 1, "Microsoft .Net Framework", ScanIssueSeverity.LOW));
+	addMatchRule(new MatchRule(IHS, 1, "IBM HTTP Server", ScanIssueSeverity.LOW));
+        addMatchRule(new MatchRule(IBM_NWEB, 1, "IBM-NWeb", ScanIssueSeverity.LOW));
+        addMatchRule(new MatchRule(IWEB, 1, "360vision CCTV Web Server", ScanIssueSeverity.LOW));
+	addMatchRule(new MatchRule(JBOSS, 1, "JBoss", ScanIssueSeverity.LOW));
+	addMatchRule(new MatchRule(JBOSS_SVNTAG, 1, "JBoss", ScanIssueSeverity.LOW));
+	addMatchRule(new MatchRule(JBOSS_TOMCAT, 1, "JBoss (Tomcat)", ScanIssueSeverity.LOW));
+	addMatchRule(new MatchRule(JBOSS_WEB, 1, "JBoss Webserver", ScanIssueSeverity.LOW));
+	addMatchRule(new MatchRule(JOOMLA, 1, "Joomla!", ScanIssueSeverity.LOW));
+	addMatchRule(new MatchRule(jQUERY_LIB, 1, "jQuery JavaScript Library", ScanIssueSeverity.INFO));
+        addMatchRule(new MatchRule(jQUERY_UITP, 1, "jQuery UI Touch Punch", ScanIssueSeverity.INFO));
+        addMatchRule(new MatchRule(jQUERY_HCE, 1, "jQuery hashchange event", ScanIssueSeverity.INFO));
+        addMatchRule(new MatchRule(jQUERY_TPS, 1, "jQuery Tiny Pub/Sub", ScanIssueSeverity.INFO));
+        addMatchRule(new MatchRule(jQUERY, 1, "jQuery", ScanIssueSeverity.INFO));
+        addMatchRule(new MatchRule(jQUERY2, 1, "jQuery", ScanIssueSeverity.INFO));
+        addMatchRule(new MatchRule(LIGHTY, 1, "lighttpd", ScanIssueSeverity.LOW));
+        addMatchRule(new MatchRule(LiteSpeed, 1, "OpenCms", ScanIssueSeverity.LOW));  
+	addMatchRule(new MatchRule(JSF, 1, "Java Server Faces", ScanIssueSeverity.LOW));
+	addMatchRule(new MatchRule(MICROSOFT_HTTPAPI, 1, "Microsoft HTTPAPI", ScanIssueSeverity.LOW));
+	addMatchRule(new MatchRule(MICROSOFT_IIS, 1, "Microsoft IIS", ScanIssueSeverity.LOW));
+	addMatchRule(new MatchRule(MOD_JK, 1, "mod_jk", ScanIssueSeverity.LOW));    
+	addMatchRule(new MatchRule(NGINX, 1, "nginx", ScanIssueSeverity.LOW));    
+        addMatchRule(new MatchRule(OPENCMS, 1, "OpenCms", ScanIssueSeverity.LOW));
+	addMatchRule(new MatchRule(MOD_SSL, 1, "mod_ssl", ScanIssueSeverity.LOW));    
+	addMatchRule(new MatchRule(MOD_PERL, 1, "mod_perl", ScanIssueSeverity.LOW));    
+        addMatchRule(new MatchRule(OMNITURE, 1, "Omniture DC (Adobe)", ScanIssueSeverity.LOW));
+	addMatchRule(new MatchRule(OPENSSL, 1, "OpenSSL", ScanIssueSeverity.LOW));    
+        addMatchRule(new MatchRule(ORACLE_IPLANET, 1, "Oracle iPlanet", ScanIssueSeverity.LOW));
+        addMatchRule(new MatchRule(ORACLE_APP_SVR, 1, "Oracle-Application-Server", ScanIssueSeverity.LOW));
+	addMatchRule(new MatchRule(ORION, 1, "Orion", ScanIssueSeverity.LOW));    
+	addMatchRule(new MatchRule(PERL, 1, "Perl", ScanIssueSeverity.LOW));    
+	addMatchRule(new MatchRule(PHP, 1, "PHP", ScanIssueSeverity.LOW));    
+        addMatchRule(new MatchRule(PHUSION, 1, "Phusion Passenger", ScanIssueSeverity.LOW));
+	addMatchRule(new MatchRule(SERVLET, 1, "Generic Java Servlet engine (possibly JBoss)", ScanIssueSeverity.LOW));    
+	addMatchRule(new MatchRule(TORNADO, 1, "Tornado Server", ScanIssueSeverity.LOW));
+        addMatchRule(new MatchRule(WAS, 1, "IBM WebSphere Application Server", ScanIssueSeverity.LOW));
+	addMatchRule(new MatchRule(X_ASP_NET, 1, "ASP.Net", ScanIssueSeverity.LOW));
+	addMatchRule(new MatchRule(X_ASP_NET_MVC, 1, "ASP.Net MVC Framework", ScanIssueSeverity.LOW));
+	addMatchRule(new MatchRule(X_OWA, 1, "Outlook Web Access", ScanIssueSeverity.LOW));
+    }
+
+    protected String getIssueName() {
+	return "Software Version Numbers Revealed";
+    }
+
+    protected String getIssueDetail(List<com.codemagi.burp.ScannerMatch> matches) {
+	StringBuilder description = new StringBuilder(matches.size() * 256);
+	description.append("The server software versions used by the application are revealed by the web server.<br>");
+	description.append("Displaying version information of software information could allow an attacker to determine which vulnerabilities are present in the software, particularly if an outdated software version is in use with published vulnerabilities.<br><br>");
+	description.append("The following software appears to be in use:<br><br>");
+	
+	for (ScannerMatch match : matches) {
+	    //add a description
+	    description.append("<li>");
+
+	    description.append(match.getType()).append(": ").append(match.getMatch());
+	}
+
+	return description.toString();
+    }
+
+    protected ScanIssueSeverity getIssueSeverity(List<com.codemagi.burp.ScannerMatch> matches) {
+	ScanIssueSeverity output = ScanIssueSeverity.INFO;
+	for (ScannerMatch match : matches) {
+	    //if the severity value of the match is higher, then update the stdout value
+	    ScanIssueSeverity matchSeverity = match.getSeverity();
+	    callbacks.printOutput("Severity: " + matchSeverity);
+	    if (matchSeverity != null && 
+		output.getValue() < matchSeverity.getValue()) {
+		
+		output = matchSeverity;
+	    }
+	    callbacks.printOutput("Output: " + output);
+	}
+	return output;
+    }
+
+    protected ScanIssueConfidence getIssueConfidence(List<com.codemagi.burp.ScannerMatch> matches) {
+	return ScanIssueConfidence.CERTAIN;
+    }
     
-    //issue severity
-    protected static final String SEVERITY_LOW = "Low";
-    protected static final String SEVERITY_INFORMATION = "Information";
-    
+    @Override
+    protected IScanIssue getScanIssue(IHttpRequestResponse baseRequestResponse, List<ScannerMatch> matches, List<int[]> startStop) {
+	return new ScanIssue(
+		baseRequestResponse, 
+		helpers,
+		callbacks, 
+		startStop, 
+		getIssueName(), 
+		getIssueDetail(matches), 
+		ScanIssueSeverity.MEDIUM.getName(), 
+		ScanIssueConfidence.FIRM.getName());
+    }
+
     //regex for server identifiers
     private static final Pattern Alterian_CME = Pattern.compile("Alterian-CME/([0-9\\.]+)");
     private static final Pattern ARR = Pattern.compile("ARR/([0-9\\.]+)");
@@ -80,368 +183,5 @@ public class BurpExtender implements IBurpExtender, IScannerCheck {
     private static final Pattern X_ASP_NET = Pattern.compile("X-AspNet-Version: ([0-9\\.]+)");
     private static final Pattern X_ASP_NET_MVC = Pattern.compile("X-AspNetMvc-Version: ([0-9\\.]+)");
     private static final Pattern X_OWA = Pattern.compile("X-OWA-Version: ([0-9\\.]+)");
-        
-    private static final List<MatchRule> rules = new ArrayList<MatchRule>();
-    static {
-	rules.add(new MatchRule(Alterian_CME, 1, "Alterian-CME"));
-        rules.add(new MatchRule(ARR, 1, "IIS Application Request Routing"));
-        rules.add(new MatchRule(ASP_NET, 1, "ASP.Net"));
-	rules.add(new MatchRule(APACHE, 1, "Apache"));
-	rules.add(new MatchRule(APACHE_COYOTE, 1, "Apache Coyote (Tomcat)"));
-	rules.add(new MatchRule(APACHE_TOMCAT, 1, "Apache Tomcat"));
-	rules.add(new MatchRule(BOA, 1, "BOA Web Server"));
-	rules.add(new MatchRule(DOT_NET_FRAMEWORK, 1, "Microsoft .Net Framework"));
-	rules.add(new MatchRule(DOT_NET_FRAMEWORK_SDK, 1, "Microsoft .Net Framework"));
-	rules.add(new MatchRule(IHS, 1, "IBM HTTP Server"));
-        rules.add(new MatchRule(IBM_NWEB, 1, "IBM-NWeb"));
-        rules.add(new MatchRule(IWEB, 1, "360vision CCTV Web Server"));
-	rules.add(new MatchRule(JBOSS, 1, "JBoss"));
-	rules.add(new MatchRule(JBOSS_SVNTAG, 1, "JBoss"));
-	rules.add(new MatchRule(JBOSS_TOMCAT, 1, "JBoss (Tomcat)"));
-	rules.add(new MatchRule(JBOSS_WEB, 1, "JBoss Webserver"));
-	rules.add(new MatchRule(JOOMLA, 1, "Joomla!"));
-	rules.add(new MatchRule(jQUERY_LIB, 1, "jQuery JavaScript Library", SEVERITY_INFORMATION));
-        rules.add(new MatchRule(jQUERY_UITP, 1, "jQuery UI Touch Punch", SEVERITY_INFORMATION));
-        rules.add(new MatchRule(jQUERY_HCE, 1, "jQuery hashchange event", SEVERITY_INFORMATION));
-        rules.add(new MatchRule(jQUERY_TPS, 1, "jQuery Tiny Pub/Sub", SEVERITY_INFORMATION));
-        rules.add(new MatchRule(jQUERY, 1, "jQuery", SEVERITY_INFORMATION));
-        rules.add(new MatchRule(jQUERY2, 1, "jQuery", SEVERITY_INFORMATION));
-        rules.add(new MatchRule(LIGHTY, 1, "lighttpd"));
-        rules.add(new MatchRule(LiteSpeed, 1, "OpenCms"));  
-	rules.add(new MatchRule(JSF, 1, "Java Server Faces"));
-	rules.add(new MatchRule(MICROSOFT_HTTPAPI, 1, "Microsoft HTTPAPI"));
-	rules.add(new MatchRule(MICROSOFT_IIS, 1, "Microsoft IIS"));
-	rules.add(new MatchRule(MOD_JK, 1, "mod_jk"));    
-	rules.add(new MatchRule(NGINX, 1, "nginx"));    
-        rules.add(new MatchRule(OPENCMS, 1, "OpenCms"));
-	rules.add(new MatchRule(MOD_SSL, 1, "mod_ssl"));    
-	rules.add(new MatchRule(MOD_PERL, 1, "mod_perl"));    
-        rules.add(new MatchRule(OMNITURE, 1, "Omniture DC (Adobe)"));
-	rules.add(new MatchRule(OPENSSL, 1, "OpenSSL"));    
-        rules.add(new MatchRule(ORACLE_IPLANET, 1, "Oracle iPlanet"));
-        rules.add(new MatchRule(ORACLE_APP_SVR, 1, "Oracle-Application-Server"));
-	rules.add(new MatchRule(ORION, 1, "Orion"));    
-	rules.add(new MatchRule(PERL, 1, "Perl"));    
-	rules.add(new MatchRule(PHP, 1, "PHP"));    
-        rules.add(new MatchRule(PHUSION, 1, "Phusion Passenger"));
-	rules.add(new MatchRule(SERVLET, 1, "Generic Java Servlet engine (possibly JBoss)"));    
-	rules.add(new MatchRule(TORNADO, 1, "Tornado Server"));
-        rules.add(new MatchRule(WAS, 1, "IBM WebSphere Application Server"));
-	rules.add(new MatchRule(X_ASP_NET, 1, "ASP.Net"));
-	rules.add(new MatchRule(X_ASP_NET_MVC, 1, "ASP.Net MVC Framework"));
-	rules.add(new MatchRule(X_OWA, 1, "Outlook Web Access"));
-    }
-    
-    
-    /**
-     * implement IBurpExtender
-     */
-    @Override
-    public void registerExtenderCallbacks(final IBurpExtenderCallbacks callbacks) {
-	// keep a reference to our callbacks object
-	this.callbacks = callbacks;
 
-	// obtain an extension helpers object
-	helpers = callbacks.getHelpers();
-
-	// set our extension name
-	callbacks.setExtensionName("Software Version Checks");
-
-	// register ourselves as a custom scanner check
-	callbacks.registerScannerCheck(this);
-	
-	//get the output stream for info messages
-	output = callbacks.getStdout();
-	
-	println("Loaded Software Version Checks");
-    }
-
-    /**
-    * implement IScannerCheck
-    */
-    @Override
-    public List<IScanIssue> doPassiveScan(IHttpRequestResponse baseRequestResponse) {
-	List<ScannerMatch> matches = new ArrayList<ScannerMatch>();
-	List<IScanIssue> issues = new ArrayList<IScanIssue>();
-
-	//get the URL of the requst
-	URL url = helpers.analyzeRequest(baseRequestResponse).getUrl();
-	println("Scanning for software version numbers: " + url.toString());
-	
-	//get the body of the response
-	byte[] responseBytes = baseRequestResponse.getResponse();
-	String response = helpers.bytesToString(responseBytes);
-	
-	//iterate through rules and check for matches
-	for (MatchRule rule : rules) {
-	    Matcher matcher = rule.getPattern().matcher(response);
-	    while (matcher.find()) {
-		println("FOUND " + rule.getType() + "!");
-		
-		//get the actual match 
-		String group;
-		if (rule.getMatchGroup() != null) {
-		    group = matcher.group(rule.getMatchGroup());
-		} else {
-		    group = matcher.group();
-		}
-
-		println("start: " + matcher.start() + " end: " + matcher.end() + " group: " + group);
-
-		matches.add(new ScannerMatch(matcher.start(), matcher.end(), group, rule.getType(), rule.getSeverity()));
-	    }
-	}
-		
-	// report the issues ------------------------
-	if (!matches.isEmpty()) {
-	    Collections.sort(matches);  //matches must be in order 
-	    StringBuilder description = new StringBuilder(matches.size() * 256);
-	    description.append("The server software versions used by the application are revealed by the web server.<br>");
-	    description.append("Displaying version information of software information could allow an attacker to determine which vulnerabilities are present in the software, particularly if an outdated software version is in use with published vulnerabilities.<br><br>");
-	    description.append("The following software appears to be in use:<br><br>");
-	    
-	    String severity = null;
-	    
-	    List<int[]> startStop = new ArrayList<int[]>(1);
-	    for (ScannerMatch match : matches) {
-		println("Processing match: " + match);
-		println("    start: " + match.getStart() + " end: " + match.getEnd() + " match: " + match.getMatch() + " match: " + match.getMatch());
-
-		//add a marker for code highlighting
-		startStop.add(new int[]{match.getStart(), match.getEnd()});
-
-		//add a description
-		description.append("<li>");
-
-		description.append(match.getType()).append(": ").append(match.getMatch());
-
-		//update the severity level
-		if (severity == null || SEVERITY_INFORMATION.equals(severity)) {
-		    severity = match.getSeverity();
-		} 
-	    }
-
-	    println("    Description: " + description.toString());
-
-	    issues.add(new CustomScanIssue(
-			baseRequestResponse.getHttpService(),
-			helpers.analyzeRequest(baseRequestResponse).getUrl(),
-			new IHttpRequestResponse[]{callbacks.applyMarkers(baseRequestResponse, null, startStop)},
-			"Software Version Numbers Revealed",
-			description.toString(),
-			severity,
-			"Certain"));
-
-	    println("issues: " + issues.size());
-
-	    return issues;
-	    
-	}
-    
-	return null;
-    }
-
-    @Override
-    public List<IScanIssue> doActiveScan(IHttpRequestResponse baseRequestResponse, IScannerInsertionPoint insertionPoint) {
-
-	return null;
-
-    }
-
-    @Override
-    public int consolidateDuplicateIssues(IScanIssue existingIssue, IScanIssue newIssue) {
-	// This method is called when multiple issues are reported for the same URL 
-	// path by the same extension-provided check. The value we return from this 
-	// method determines how/whether Burp consolidates the multiple issues
-	// to prevent duplication
-	//
-	// Since the issue name is sufficient to identify our issues as different,
-	// if both issues have the same name, only report the existing issue
-	// otherwise report both issues
-	if (existingIssue.getIssueDetail().equals(newIssue.getIssueDetail())) {
-	    println("DUPLICATE ISSUE! Consolidating...");
-	    return -1;
-	} else {
-	    return 0;
-	}
-    }
-    
-    private void println(String toPrint) {
-	try {
-	    output.write(toPrint.getBytes());
-	    output.write("\n".getBytes());
-	    output.flush();
-	} catch (IOException ioe) {
-	    ioe.printStackTrace();
-	} 
-    }
-}
-
-
-
-/**
- * class implementing IScanIssue to hold our custom scan issue details
- */
-class CustomScanIssue implements IScanIssue {
-
-    private IHttpService httpService;
-    private URL url;
-    private IHttpRequestResponse[] httpMessages;
-    private String name;
-    private String detail;
-    private String severity;
-    private String confidence;
-
-    public CustomScanIssue(
-	    IHttpService httpService,
-	    URL url,
-	    IHttpRequestResponse[] httpMessages,
-	    String name,
-	    String detail,
-	    String severity,
-	    String confidence) {
-	this.httpService = httpService;
-	this.url = url;
-	this.httpMessages = httpMessages;
-	this.name = name;
-	this.detail = detail;
-	this.severity = severity;
-	this.confidence = confidence;
-    }
-
-    @Override
-    public URL getUrl() {
-	return url;
-    }
-
-    @Override
-    public String getIssueName() {
-	return name;
-    }
-
-    @Override
-    public int getIssueType() {
-	return 0;
-    }
-
-    @Override
-    public String getSeverity() {
-	return severity;
-    }
-
-    @Override
-    public String getConfidence() {
-	return confidence;
-    }
-
-    @Override
-    public String getIssueBackground() {
-	return null;
-    }
-
-    @Override
-    public String getRemediationBackground() {
-	return null;
-    }
-
-    @Override
-    public String getIssueDetail() {
-	return detail;
-    }
-
-    @Override
-    public String getRemediationDetail() {
-	return null;
-    }
-
-    @Override
-    public IHttpRequestResponse[] getHttpMessages() {
-	return httpMessages;
-    }
-
-    @Override
-    public IHttpService getHttpService() {
-	return httpService;
-    }
-
-}
-
-
-class ScannerMatch implements Comparable<ScannerMatch> {
-
-    private Integer start;
-    private int end;
-    private String match;
-    private String type;
-    private String severity;
-
-    public ScannerMatch(int start, int end, String match, String type, String severity) {
-	this.start = start;
-	this.end = end;
-	this.match = match;
-	this.type = type;
-	this.severity = severity;
-    }
-
-    public int getStart() {
-	return start;
-    }
-
-    public int getEnd() {
-	return end;
-    }
-
-    public String getMatch() {
-	return match;
-    }
-
-    public String getType() {
-	return type;
-    }    
-    
-    public String getSeverity() {
-	return severity;
-    }
-    
-    @Override
-    public int compareTo(ScannerMatch m) {
-        return start.compareTo(m.getStart());
-    }
-}
-
-
-class MatchRule {
-    private Pattern pattern;
-    private Integer matchGroup;
-    private String type;
-    private String severity = BurpExtender.SEVERITY_LOW;
-
-    public MatchRule(Pattern pattern, Integer matchGroup, String type) {
-	this.pattern = pattern;
-	this.matchGroup = matchGroup;
-	this.type = type;
-    }
-
-    public MatchRule(Pattern pattern, Integer matchGroup, String type, String severity) {
-	this.pattern = pattern;
-	this.matchGroup = matchGroup;
-	this.type = type;
-	this.severity = severity;
-    }
-    
-    public Pattern getPattern() {
-	return pattern;
-    }
-
-    public Integer getMatchGroup() {
-	return matchGroup;
-    }
-
-    public String getType() {
-	return type;
-    }
-
-    public String getSeverity() {
-	return severity;
-    }
 }
